e2c4d9412fe446e3353d8b3bc88d1c6be5dd9c31
QuantumChemist <52951132+QuantumChemist@users.noreply.github.com>
2025-12-04 12:45:07 +0100

    Add initial implementation for day 4 solution; read input from day4.txt

---
+++ day4/day4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <set>
+#include <string>
+
+int main() {
+    std::ifstream infile("day4.txt");
+    std::string line;
+
+    while (std::getline(infile, line)) {
+
+    }
+    infile.close();
+    return 0;
+}
