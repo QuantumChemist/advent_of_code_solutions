4dfe7dcab4a30bc51dd75eaa52304ff75965a8bf
QuantumChemist <52951132+QuantumChemist@users.noreply.github.com>
2025-12-05 10:46:15 +0100

    Add initial file reading logic for days 5 to 12

---
+++ day9/day9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <set>
+#include <string>
+
+
+int main() {
+    std::ifstream infile("day9.txt");
+    std::string line;
+
+    while (std::getline(infile, line)) {
+
+    }
+    
+    infile.close();
+    return 0;
+}
