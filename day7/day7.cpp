7f05901db859a7552954dd185ee6c08a10dd2a88
QuantumChemist <52951132+QuantumChemist@users.noreply.github.com>
2025-12-07 14:19:44 +0100

    Refactor counting logic to improve clarity and maintainability

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -9,11 +9,17 @@
 int main() {
     std::ifstream infile("day7.txt");
     std::string line;
+    long long splitter_count = 0;
 
     while (std::getline(infile, line)) {
-
+    // we count the number of ^ charcters in each line
+        for (char c : line) {
+            if (c == '^') {
+                splitter_count++;
+            }
+        }
     }
-    
     infile.close();
+
     return 0;
 }
