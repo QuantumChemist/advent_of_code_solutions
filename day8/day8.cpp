46a13f5c87185fb3f005943e9c9b253ff6fa17d9
QuantumChemist <52951132+QuantumChemist@users.noreply.github.com>
2025-12-08 15:46:22 +0100

    refactor: reorganize code for better readability and structure

--- day8/day8.cpp
+++ day8/day8.cpp
@@ -4,16 +4,24 @@
 #include <string>
 #include <set>
 #include <string>
+#include <vector>
 
 
 int main() {
     std::ifstream infile("day8.txt");
     std::string line;
+    std::vector<std::vector<long long>> junction_box;
 
     while (std::getline(infile, line)) {
-
+        std::istringstream iss(line);
+        std::vector<long long> row;
+        long long number;
+        while (iss >> number) {
+            row.push_back(number);
+        }
+        junction_box.push_back(row);
     }
-    
     infile.close();
+
     return 0;
 }
